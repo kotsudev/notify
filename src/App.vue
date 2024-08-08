d9c16a13832693fbcb1abad062765607c4dffd94
kotsu <antontamazlukar@gmail.com>
2024-08-08 17:02:05 +0200

    Add ability to change panes width by moving the pane splitter

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,73 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
 
-<template></template>
+const container = ref();
+const splitter = ref();
+const leftPane = ref();
+const rightPane = ref();
+const isDragging = ref(false);
 
-<style scoped></style>
+onMounted(() => {
+  splitter.value.addEventListener("mousedown", () => {
+    isDragging.value = true;
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+  });
+
+  function onMouseMove(e: MouseEvent) {
+    if (!isDragging.value) return;
+
+    const containerRect = container.value.getBoundingClientRect();
+    const offset = e.clientX - containerRect.left;
+    const totalWidth = containerRect.width;
+    const PADDING_OFFSET = 12;
+    const WIDTH_THRESHOLD = 320;
+
+    const leftWidth = offset;
+    const rightWidth = totalWidth - offset - splitter.value.offsetWidth;
+
+    if (leftWidth <= WIDTH_THRESHOLD || rightWidth <= WIDTH_THRESHOLD) return;
+
+    leftPane.value.style.width = `${leftWidth}px`;
+    rightPane.value.style.width = `${rightWidth - PADDING_OFFSET}px`;
+  }
+
+  function onMouseUp() {
+    isDragging.value = false;
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseup", onMouseUp);
+  }
+});
+</script>
+
+<template>
+  <div class="container" ref="container">
+    <div class="pane" ref="leftPane"></div>
+    <div class="splitter" ref="splitter" />
+    <div class="pane" ref="rightPane"></div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  gap: 6px;
+}
+
+.pane {
+  background-color: rgba(255, 255, 255, 0.4);
+  width: 50%;
+  height: 100%;
+  border-radius: 1rem;
+}
+
+.splitter {
+  background-color: rgba(255, 255, 255, 0.4);
+  cursor: ew-resize;
+  height: 60px;
+  width: 4px;
+  border-radius: 2px;
+}
+</style>
