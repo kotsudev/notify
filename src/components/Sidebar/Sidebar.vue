192be363896aad93e2e44096251636ca9cd30568
kotsu <antontamazlukar@gmail.com>
2024-08-13 19:27:27 +0200

    Add SidebarHeader component

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import SidebarHeader from "@/components/Sidebar/SidebarHeader.vue";
 </script>
 
 <template>
   <div class="sidebar">
+    <SidebarHeader />
   </div>
 </template>
 
---
+++ src/components/Sidebar/SidebarHeader.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Button from "@/components/shared/Button/Button.vue";
+</script>
+
+<template>
+  <div class="sidebar-header">
+    <Button>Edit</Button>
+    <span class="counter">7 items</span>
+    <Button>New</Button>
+  </div>
+</template>
+
+<style scoped>
+.sidebar-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.counter {
+  color: #000;
+  font-size: 14px;
+  cursor: default;
+}
+</style>
