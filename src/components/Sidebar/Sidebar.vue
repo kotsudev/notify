447457e8c081465404b74124e72361d44a73a853
kotsu <antontamazlukar@gmail.com>
2024-08-25 18:00:26 +0200

    Improve Sidebar component

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,17 +1,43 @@
 <script setup lang="ts">
 import SidebarHeader from "@/components/Sidebar/SidebarHeader.vue";
+import SidebarSelectors from "@/components/Sidebar/SidebarSelectors.vue";
+import Note from "@/components/Note/Note.vue";
 </script>
 
 <template>
-  <div class="sidebar">
-    <SidebarHeader />
+  <SidebarHeader />
+  <SidebarSelectors />
+  <span class="date">Today</span>
+  <div class="scroll-wrapper">
+    <div class="notes">
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.sidebar {
+.date {
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 12px;
+  font-weight: 300;
+  text-align: center;
+  margin: 9px 0 18px;
+}
+
+.scroll-wrapper {
+  overflow: scroll;
+  border-radius: 8px;
+}
+
+.notes {
   display: flex;
   flex-direction: column;
-  padding: 24px;
+  gap: 8px;
 }
 </style>
