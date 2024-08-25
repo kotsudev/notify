7aa015c69a27548cd7013035b9a672770d4af4f6
kotsu <antontamazlukar@gmail.com>
2024-08-25 17:59:33 +0200

    Add SidebarSelectors component

---
+++ src/components/Sidebar/SidebarSelectors.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Input from "@/components/shared/Input/Input.vue";
+import Selector from "@/components/shared/Selector/Selector.vue";
+</script>
+
+<template>
+  <div class="selectors">
+    <Input />
+    <Selector />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.selectors {
+  display: flex;
+  flex-direction: column;
+  border-radius: 12px;
+  background-color: rgba(255, 255, 255, 0.15);
+  padding: 8px;
+  margin-top: 24px;
+  gap: 8px;
+}
+</style>
