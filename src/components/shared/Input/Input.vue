31b360593f61a8b9dda78e2806e53f2be433f2e7
kotsu <antontamazlukar@gmail.com>
2024-08-25 17:56:42 +0200

    Add shared Input component

---
+++ src/components/shared/Input/Input.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <input class="input" placeholder="Search" />
+</template>
+
+<style lang="scss" scoped>
+.input {
+  min-height: 28px;
+  font-size: 16px;
+  border-radius: 8px;
+  background-color: rgba(255, 255, 255, 0.3);
+  border: 1px solid transparent;
+  color: #000;
+  text-align: center;
+}
+</style>
